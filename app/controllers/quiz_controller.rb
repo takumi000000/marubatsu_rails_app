3ad2401c0b3503a4d7abfbea5bffab4719cac926
takumi <takumindex@gmail.com>
2022-01-31 15:01:30 +0900

    Add result_flag

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -18,8 +18,10 @@ class QuizController < ApplicationController
 
     if @flag == @select
       puts "正解です"
+      redirect_to quiz_result_path(params: {seigo: 1})
     else
       puts "不正解です"
+      redirect_to quiz_result_path(params: {seigo: 0})
     end
 
 
@@ -29,5 +31,11 @@ class QuizController < ApplicationController
   end
 
   def result
+    if params[:seigo].to_i == 1
+      @result_flag = "正解です"
+    else
+      @result_flag = "不正解です"
+    end
+
   end
 end
